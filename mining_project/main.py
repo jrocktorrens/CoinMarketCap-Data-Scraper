a5940aeac64767e1f8a4672b557101ffdd019b68
Yossi Golan <yossigolan@gmail.com>
2021-06-22 14:16:13 +0300

    Wraped in function and added comments

--- mining_project/main.py
+++ mining_project/main.py
@@ -1,51 +1,73 @@
 """
 Data mining Project
 
+Contributors:
+Aviad Haviv ()
+Yossi Golan (yossigolan@gmail.com)
 
 """
 
 import requests
 from bs4 import BeautifulSoup
 
+SITE_URL = "https://coinmarketcap.com/currencies/"
 
-r = requests.get("https://coinmarketcap.com/currencies/Bitcoin/")
 
-soup = BeautifulSoup(r.text,'html.parser')
-# price_statistics = soup.find('div', class_='sc-16r8icm-0 dxttqv')
-# for tr in price_statistics.table.tbody:
-#     table_key = tr.th.text
-#     table_value = tr.td.text
-#     print(table_key, table_value)
-# print(price_statistics.prettify())
+def get_coin_info(coin_name):
+    """
+    Scrap all the information of a crypto coin
+    :param coin_name: the name of the coin out of the following list:
+    'bitcoin', 'ethereum', 'tether', 'binance-coin', 'cardano', 'dogecoin', 'xrp', 'usd-coin',
+    'polkadot-new', 'uniswap', 'litecoin', 'solana', 'theta'
+    :return: None. just prints the information to the screen
+    """
+    if coin_name not in ['bitcoin', 'ethereum', 'tether', 'binance-coin', 'cardano',
+                         'dogecoin', 'xrp', 'usd-coin', 'polkadot-new', 'uniswap', 'litecoin',
+                         'solana', 'theta']:
+        raise ValueError(f"Error! Coin {coin_name} is not supported!")
 
-price_statistics = soup.find_all('div', class_='sc-16r8icm-0 dxttqv')
-div_counter = 0
-for div in price_statistics:
-    if div_counter <= 4:
-        for table in div:
-            for tr in table.tbody:
-                table_key = tr.th.text
-                table_value = tr.td.text
-                print(table_key, table_value)
-        div_counter += 1
-coin_text = soup.find('div', class_='sc-16r8icm-0 coDTMj')
-print(coin_text.find('div', class_='about___1OuKY').div.div.p.text)
+    try:
+        r = requests.get(SITE_URL + coin_name + "/")
+        soup = BeautifulSoup(r.text, 'html.parser')
+    except ConnectionError:
+        print(f"Something on the website went wrong reading {coin_name} information.")
+        print("Please try again later...")
 
-for p in coin_text.find('div', class_='about___1OuKY').div.div.div.div:
-    print(p.text)
-#print(coin_text.find('div', class_='about___1OuKY').div.div.div.div.p.text)
 
-"""
-article = soup.find('div', class='article')
-print(article)
-headline = article.h2.a.text
-print(headline)
-summary = article.p.text
-print(summary)
+    print("\n", f"Here is the latest price statistics of {coin_name}:")
+    print(f"---------------------------------------" + '-'*(len(coin_name)+1))
 
-for article in soup.find('div', class='article')
-    headline = article.h2.a.text
-    print(headline)
-    summary = article.p.text
-    print(summary)
-"""
+    # Get all divs that contain coin statistics
+    try:
+        price_statistics = soup.find_all('div', class_='sc-16r8icm-0 dxttqv')
+        div_counter = 0
+        for div in price_statistics:
+            # Get first 4 tables
+            if div_counter <= 4:
+                for table in div:
+                    for tr in table.tbody:
+                        table_key = tr.th.text
+                        table_value = tr.td.text
+                        print(table_key, table_value)
+                div_counter += 1
+
+        print("\n", f"Here is a live data summery of {coin_name}:")
+        print(f"-------------------------------------" + '-'*(len(coin_name)+1))
+
+        # Get updated coin information summary
+        coin_text = soup.find('div', class_='sc-16r8icm-0 coDTMj')
+        print(coin_text.find('div', class_='about___1OuKY').div.div.p.text)
+
+        print("\n", f"What is {coin_name}?")
+        print(f"--------------" + '-'*(len(coin_name)+1))
+
+        # Get 'What is' coin information that includes coin history and all related information.
+        for p in coin_text.find('div', class_='about___1OuKY').div.div.div.div:
+            print(p.text)
+    except ConnectionError:
+        print(f"Can't read {coin_name} information.")
+        print("Please try again later...")
+
+
+if __name__ == '__main__':
+    get_coin_info('ethereum')
