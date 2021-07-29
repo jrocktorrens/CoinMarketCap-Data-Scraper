3c38e7552681ca387b84c27596f9786b3a4fb384
yossigolan <yossigolan@gmail.com>
2021-07-29 15:29:37 +0300

    Removed commented lines from sql_handler.py

--- mining_project/sql_handler.py
+++ mining_project/sql_handler.py
@@ -26,10 +26,6 @@ def setup_coins_table(database, sql_sentence):
     Create the coins table if not exist
     :return: nothing
     """
-    # sql_sentence = "CREATE TABLE IF NOT EXISTS coins (" \
-    #                "coin_id INT PRIMARY KEY," \
-    #                "name TEXT" \
-    #                ");"
     run_sql(database, sql_sentence)
 
 
@@ -38,38 +34,8 @@ def setup_coin_price_today_table(database, sql_sentence):
     Create today's coin price table
     :return: nothing
     """
-    # sql_sentence = "CREATE TABLE IF NOT EXISTS coin_price_today (" \
-    #                "id INT AUTO_INCREMENT PRIMARY KEY," \
-    #                "coin_id INT," \
-    #                "price FLOAT," \
-    #                "price_change FLOAT," \
-    #                "low FLOAT," \
-    #                "high FLOAT," \
-    #                "volume FLOAT," \
-    #                "volume_market_cap FLOAT," \
-    #                "market_dominance FLOAT," \
-    #                "market_cap FLOAT," \
-    #                "fully_diluted_market_cap FLOAT," \
-    #                "market_rank INT," \
-    #                "data_summery TEXT" \
-    #                ");"
     run_sql(database, sql_sentence[0])
-    # sql_sentence = "ALTER TABLE `coin_price_today` " \
-    #                "ADD FOREIGN KEY (`coin_id`) REFERENCES" \
-    #                " `coins` (`coin_id`);"
     run_sql(database, sql_sentence[1])
-    # sql_sentence = "ALTER TABLE coin_price_today ADD UNIQUE (" \
-    #                "coin_id," \
-    #                "price," \
-    #                "price_change," \
-    #                "low," \
-    #                "high," \
-    #                "volume," \
-    #                "volume_market_cap," \
-    #                "market_dominance," \
-    #                "market_cap," \
-    #                "fully_diluted_market_cap," \
-    #                "market_rank);"
     run_sql(database, sql_sentence[2])
 
 
@@ -79,30 +45,8 @@ def setup_coin_price_yesterday_table(database, sql_sentence):
     :return: nothing
     """
     try:
-        # sql_sentence = "CREATE TABLE IF NOT EXISTS coin_price_yesterday (" \
-        #                "id INT AUTO_INCREMENT PRIMARY KEY," \
-        #                "coin_id INT UNIQUE," \
-        #                "low FLOAT UNIQUE," \
-        #                "high FLOAT UNIQUE," \
-        #                "open FLOAT UNIQUE," \
-        #                "close FLOAT UNIQUE," \
-        #                "coin_change FLOAT UNIQUE," \
-        #                "volume FLOAT UNIQUE" \
-        #                ");"
         run_sql(database, sql_sentence[0])
-        # sql_sentence = "ALTER TABLE `coin_price_yesterday` " \
-        #                "ADD FOREIGN KEY (`coin_id`) REFERENCES" \
-        #                " `coins` (`coin_id`);"
         run_sql(database, sql_sentence[1])
-        # sql_sentence = "ALTER TABLE coin_price_yesterday ADD UNIQUE ("\
-        #                "coin_id," \
-        #                "low," \
-        #                "high," \
-        #                "open," \
-        #                "close," \
-        #                "coin_change," \
-        #                "volume" \
-        #                ");"
         run_sql(database, sql_sentence[2])
     except Exception as ex:
         print(f"Error")
@@ -113,36 +57,8 @@ def setup_coin_price_history_table(database, sql_sentence):
     Create price history table
     :return: nothing
     """
-    # sql_sentence = "CREATE TABLE IF NOT EXISTS coin_price_history (" \
-    #                "id INT AUTO_INCREMENT PRIMARY KEY," \
-    #                "coin_id INT," \
-    #                "7d_low FLOAT," \
-    #                "7d_high FLOAT," \
-    #                "30d_low FLOAT," \
-    #                "30d_high FLOAT," \
-    #                "90d_low FLOAT," \
-    #                "90d_high FLOAT," \
-    #                "52w_low FLOAT," \
-    #                "52w_high FLOAT," \
-    #                "roi FLOAT" \
-    #                ");"
     run_sql(database, sql_sentence[0])
-    # sql_sentence = "ALTER TABLE `coin_price_history` " \
-    #                "ADD FOREIGN KEY (`coin_id`) REFERENCES" \
-    #                " `coins` (`coin_id`);"
     run_sql(database, sql_sentence[1])
-    # sql_sentence = "ALTER TABLE coin_price_history ADD UNIQUE (" \
-    #                "coin_id," \
-    #                "7d_low," \
-    #                "7d_high," \
-    #                "30d_low," \
-    #                "30d_high," \
-    #                "90d_low," \
-    #                "90d_high," \
-    #                "52w_low," \
-    #                "52w_high," \
-    #                "roi" \
-    #                ");"
     run_sql(database, sql_sentence[2])
 
 
@@ -151,18 +67,8 @@ def setup_coin_information(database, sql_sentence):
     Create coin information table
     :return: nothing
     """
-    # sql_sentence = "CREATE TABLE IF NOT EXISTS coin_information (" \
-    #                "coin_id INT AUTO_INCREMENT PRIMARY KEY," \
-    #                "info TEXT" \
-    #                ");"
     run_sql(database, sql_sentence[0])
-    # sql_sentence = "ALTER TABLE `coin_information` " \
-    #                "ADD FOREIGN KEY (`coin_id`) REFERENCES" \
-    #                " `coins` (`coin_id`);"
     run_sql(database, sql_sentence[1])
-    # sql_sentence = "ALTER TABLE coin_information ADD UNIQUE (" \
-    #                "coin_id" \
-    #                ");"
     run_sql(database, sql_sentence[2])
 
 
@@ -172,7 +78,6 @@ def setup_sql_database(database, sql_sentence):
     and then create all tables
     :return: nothing
     """
-    # sql_sentence = "CREATE DATABASE IF NOT EXISTS crypto;"
     run_sql(None, sql_sentence[0])
 
     setup_coins_table(database, sql_sentence[1])
@@ -197,11 +102,6 @@ def update_coins_table(database, coin_id, coin_name, sql_sentence):
                                      cursorclass=pymysql.cursors.DictCursor)
         with connection:
             with connection.cursor() as cursor:
-                # sql_sentence = "INSERT INTO coins (" \
-                #                     "coin_id," \
-                #                     "name" \
-                #                     ") VALUES (%s, %s);"
-                # Write a single record
                 cursor.execute(sql_sentence, (coin_id, coin_name))
                 connection.commit()
                 return cursor.fetchall()
@@ -225,21 +125,6 @@ def update_coin_price_today_table(database, current_data, conf, sql_sentence):
                                      cursorclass=pymysql.cursors.DictCursor)
         with connection:
             with connection.cursor() as cursor:
-                # sql_sentence = "INSERT INTO coin_price_today (" \
-                #                 "coin_id," \
-                #                 "price," \
-                #                 "price_change," \
-                #                 "low," \
-                #                 "high," \
-                #                 "volume," \
-                #                 "volume_market_cap," \
-                #                 "market_dominance," \
-                #                 "market_cap," \
-                #                 "fully_diluted_market_cap," \
-                #                 "market_rank," \
-                #                 "data_summery" \
-                #                 ") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
-
                 cursor.execute(sql_sentence, (current_data[conf.table_key.coin_id],
                                               current_data[conf.table_key.today_price],
                                               current_data[conf.table_key.today_price_change],
@@ -268,15 +153,6 @@ def update_coin_price_yesterday_table(database, current_data, conf, sql_sentence
                                      cursorclass=pymysql.cursors.DictCursor)
         with connection:
             with connection.cursor() as cursor:
-                # sql_sentence = "INSERT INTO coin_price_yesterday (" \
-                #                 "coin_id," \
-                #                 "low," \
-                #                 "high," \
-                #                 "open," \
-                #                 "close," \
-                #                 "coin_change," \
-                #                 "volume" \
-                #                 ") VALUES (%s, %s, %s, %s, %s, %s, %s);"
                 cursor.execute(sql_sentence, (current_data[conf.table_key.coin_id],
                                               current_data[conf.table_key.yesterday_low],
                                               current_data[conf.table_key.yesterday_high],
@@ -301,18 +177,6 @@ def update_coin_price_history_table(database, current_data, conf, sql_sentence):
                                      cursorclass=pymysql.cursors.DictCursor)
         with connection:
             with connection.cursor() as cursor:
-                # sql_statement = "INSERT INTO coin_price_history (" \
-                #                 "coin_id," \
-                #                 "7d_low," \
-                #                 "7d_high," \
-                #                 "30d_low," \
-                #                 "30d_high," \
-                #                 "90d_low," \
-                #                 "90d_high," \
-                #                 "52w_low," \
-                #                 "52w_high," \
-                #                 "roi" \
-                #                 ") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
                 cursor.execute(sql_sentence, (current_data[conf.table_key.coin_id],
                                               current_data[conf.table_key.history_7d_low],
                                               current_data[conf.table_key.history_7d_high],
@@ -339,10 +203,6 @@ def update_coin_information_table(database, current_data, conf, sql_sentence):
                                      cursorclass=pymysql.cursors.DictCursor)
         with connection:
             with connection.cursor() as cursor:
-                # sql_statement = "INSERT INTO coin_information (" \
-                #                 "coin_id," \
-                #                 "info" \
-                #                 ") VALUES (%s, %s);"
                 cursor.execute(sql_sentence, (current_data[conf.table_key.coin_id],
                                               current_data[conf.table_key.coin_about]))
                 connection.commit()
