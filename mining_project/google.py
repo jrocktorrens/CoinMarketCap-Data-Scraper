3fcdd18806866c15d68e86d4317606b2b686c0ac
yossigolan <yossigolan@gmail.com>
2021-08-05 18:23:32 +0300

    Fixed google api error 429 (search amount exceeded)

--- mining_project/google.py
+++ mining_project/google.py
@@ -6,11 +6,14 @@ def search_google(query, api_key, search_engine_id):
     start = (page - 1) * 10 + 1
     url = f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={search_engine_id}&q={query}&start={start}"
     data = requests.get(url).json()
-    search_items = data['items']
-    search_info = data['searchInformation']
-    total_results = search_info['totalResults']
-    web_sites = ''
-    for i, search_item in enumerate(search_items):
-        web_sites += search_item['link'] + '\n'
-
-    return total_results, web_sites
+    error = data['error']
+    if error['code'] !=  429:
+        search_items = data['items']
+        search_info = data['searchInformation']
+        total_results = search_info['totalResults']
+        web_sites = ''
+        for i, search_item in enumerate(search_items):
+            web_sites += search_item['link'] + '\n'
+        return total_results, web_sites
+    else:
+        return None, None
