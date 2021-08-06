460018a3261242930c6b6a7e052bc54b33f4ec72
yossigolan <yossigolan@gmail.com>
2021-08-06 08:13:10 +0300

    Added logging to google api

--- mining_project/google.py
+++ mining_project/google.py
@@ -1,5 +1,5 @@
 import requests
-
+import logging
 
 def search_google(query, api_key, search_engine_id):
     page = 1
@@ -7,13 +7,15 @@ def search_google(query, api_key, search_engine_id):
     url = f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={search_engine_id}&q={query}&start={start}"
     data = requests.get(url).json()
     error = data['error']
-    if error['code'] !=  429:
+    if error['code'] != 429:
         search_items = data['items']
         search_info = data['searchInformation']
         total_results = search_info['totalResults']
         web_sites = ''
         for i, search_item in enumerate(search_items):
             web_sites += search_item['link'] + '\n'
+        logging.info('Google api succeeded reading info from google!')
         return total_results, web_sites
     else:
+        logging.error('Google api Error! Limit of searches exceeded.')
         return None, None
